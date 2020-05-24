1d614f960d007f7f21eaceeac249993110ccf940
BrettyUK <ryanbrett@Ryans-MacBook-Pro.local>
2020-05-24 20:17:32 +0100

    Add loading spinner

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 // Listen for submit
-document
-  .getElementById('loan-form')
-  .addEventListener('submit', calculateResults);
+document.getElementById('loan-form').addEventListener('submit', function (e) {
+  // Hide results
+  document.getElementById('results').style.display = 'none';
+  // Show loader
+  document.getElementById('loading').style.display = 'block';
+  setTimeout(calculateResults, 2000);
+  e.preventDefault();
+});
 
 // Calculate results
 function calculateResults(e) {
@@ -26,15 +31,23 @@ function calculateResults(e) {
     monthlyPayment.value = monthly.toFixed(2);
     totalPayment.value = (monthly * calculatedPayments).toFixed(2);
     totalInterest.value = (monthly * calculatedPayments - principal).toFixed(2);
+    // Show results
+    document.getElementById('results').style.display = 'block';
+    // Hide loader
+    document.getElementById('loading').style.display = 'none';
   } else {
     showError('Please check your numbers');
+    document.getElementById('loading').style.display = 'none';
   }
-  e.preventDefault();
 }
 
 // Show Error
 
 function showError(error) {
+  // Show results
+  document.getElementById('results').style.display = 'none';
+  // Hide loader
+  document.getElementById('loading').style.display = 'none';
   // Create a div
   const errorDiv = document.createElement('div');
 
